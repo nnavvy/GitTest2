f4bf1aca9fcc3abbc992cf11fda08a4935f33661
nnavvy <nnavvy@gmail.com>
2012-07-09 21:54:05 +0200

    SecondRepoCommit

--- ItWillWorkEventually/src/main/java/git/ItWillWorkEventually/App.java
+++ ItWillWorkEventually/src/main/java/git/ItWillWorkEventually/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "First real change!" );
+        System.out.println("Change on abother local repo!");
     }
 }
